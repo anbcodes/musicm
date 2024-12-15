72f9037bfc01fa23e5036771aec88c405144d1a6
Anbcodes <me@anb.codes>
2024-12-14 19:17:21 -0500

    fixed safari ios

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import preact from '@preact/preset-vite';
 import basicSSL from '@vitejs/plugin-basic-ssl'
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact(), basicSSL()],
+	plugins: [preact()],
 });
