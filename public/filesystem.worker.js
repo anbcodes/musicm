57bdf130be457c4a15370373631b6cba8f7dd835
Anbcodes <me@anb.codes>
2024-12-14 23:05:55 -0500

    fixed sync issue and spa

--- public/filesystem.worker.js
+++ public/filesystem.worker.js
@@ -1,17 +1,25 @@
+let lastSave = new Promise(resolve => resolve());
+
 onmessage = async function(e) {
   console.log('Worker: Message received from main script');
-  let dir = await this.navigator.storage.getDirectory();
-  const p = e.data.project;
-  const id = e.data.id;
-  const file = await dir.getFileHandle(`project-${p.id}.json`, {create: true});
-  
+  await lastSave;
+  lastSave = new Promise(async (resolve) => {
+    let dir = await this.navigator.storage.getDirectory();
+    const p = e.data.project;
+    const id = e.data.id;
+    const file = await dir.getFileHandle(`project-${p.id}.json`, {create: true});
 
-  if (e.data.type === 'save') {
-    // @ts-ignore
-    const writer = await file.createSyncAccessHandle();
-    writer.write(new TextEncoder().encode(JSON.stringify(p)));
-    writer.close();
-    console.log("Saved");
-    this.postMessage({type: "saveR", id})
-  }
+    if (e.data.type === 'save') {
+      // @ts-ignore
+      const writer = await file.createSyncAccessHandle();
+      writer.truncate(0);
+      writer.write(new TextEncoder().encode(JSON.stringify(p)));
+      writer.close();
+      console.log("Saved");
+      this.postMessage({type: "saveR", id})
+    }
+
+    resolve();
+  })
+  
 }
